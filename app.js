bb3410b250a6ab4bd0a49e1c23a0d99e51d95875
SeannRJ <jaspersean129@gmail.com>
2023-04-04 14:49:58 -0700

    added 2 more boxes

--- app.js
+++ app.js
@@ -11,15 +11,28 @@ boxToggle.addEventListener('click', () => {
     }
   });
 
-// const boxToggleTwo = document.querySelector('#toggle-two');
-// const boxContentTwo = document.querySelector('#content-two');
+const boxToggleTwo = document.querySelector('#toggle-two');
+const boxContentTwo = document.querySelector('#content-two');
 
-// boxToggle.addEventListener('click', () => {
-//     if (boxContent.style.display === 'none') {
-//       boxContent.style.display = 'block';
-//       boxToggle.textContent = 'Click';
-//     } else {
-//       boxContent.style.display = 'none';
-//       boxToggle.textContent = 'Click';
-//     }
-//   });
+boxToggleTwo.addEventListener('click', () => {
+    if (boxContentTwo.style.display === 'none') {
+      boxContentTwo.style.display = 'block';
+      boxToggleTwo.textContent = 'Click';
+    } else {
+      boxContentTwo.style.display = 'none';
+      boxToggleTwo.textContent = 'Click';
+    }
+  });
+
+  const boxToggleThree = document.querySelector('#toggle-three');
+  const boxContentThree = document.querySelector('#content-three');
+  
+  boxToggleThree.addEventListener('click', () => {
+      if (boxContentThree.style.display === 'none') {
+        boxContentThree.style.display = 'block';
+        boxToggleThree.textContent = 'Click';
+      } else {
+        boxContentThree.style.display = 'none';
+        boxToggleThree.textContent = 'Click';
+      }
+    });
