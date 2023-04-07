1030c0c0b201d950a093d55e283185264f1ec927
SeannRJ <jaspersean129@gmail.com>
2023-04-07 08:43:11 -0700

    changing everything

--- app.js
+++ app.js
@@ -1,41 +1,14 @@
-const boxToggle = document.querySelector('#toggle');
-const boxContent = document.querySelector('#content');
+const colorSwapButton = document.querySelector(".color-swap-button");
 
-boxToggle.addEventListener('click', () => {
-    if (boxContent.style.display === 'none') {
-      boxContent.style.display = 'block';
-      boxToggle.textContent = 'Click Here';
-    } else {
-      boxContent.style.display = 'none';
-      boxToggle.textContent = 'Click Here';
-    }
-  });
+colorSwapButton.addEventListener("click", function() {
+	const body = document.querySelectorAll("body")[0];
 
-const boxToggleTwo = document.querySelector('#toggle-two');
-const boxContentTwo = document.querySelector('#content-two');
-
-boxToggleTwo.addEventListener('click', () => {
-    if (boxContentTwo.style.display === 'none') {
-      boxContentTwo.style.display = 'block';
-      boxToggleTwo.textContent = 'Click';
-    } else {
-      boxContentTwo.style.display = 'none';
-      boxToggleTwo.textContent = 'Click';
-    }
-  });
-
-  const boxToggleThree = document.querySelector('#toggle-three');
-  const boxContentThree = document.querySelector('#content-three');
-  
-  boxToggleThree.addEventListener('click', () => {
-      if (boxContentThree.style.display === 'none') {
-        boxContentThree.style.display = 'block';
-        boxToggleThree.textContent = 'Click';
-      } else {
-        boxContentThree.style.display = 'none';
-        boxToggleThree.textContent = 'Click';
-      }
-    });
-
-// const buttToggle = document.querySelector('butt');
-// buttToggle.addEventListener
+	const currentBgColor = body.style.backgroundColor;
+	if (currentBgColor === "rgb(165, 165, 165)") {
+		body.style.backgroundColor = "#000000";
+		body.style.color = "#A5A5A5";
+	} else {
+		body.style.backgroundColor = "#A5A5A5";
+		body.style.color = "#000000";
+	}
+});
