25eee136ddbffcffa5b13ec50fa9d8487c598c9c
SeannRJ <jaspersean129@gmail.com>
2023-04-07 10:59:17 -0700

    added up and down buttons

--- app.js
+++ app.js
@@ -12,3 +12,20 @@ colorSwapButton.addEventListener("click", function() {
 		body.style.color = "#000000";
 	}
 });
+
+const sections = document.querySelectorAll('.section');
+let currentIndex = 0;
+
+document.querySelector('.scroll-down').addEventListener('click', function() {
+  if (currentIndex < sections.length - 1) {
+    currentIndex++;
+    sections[currentIndex].scrollIntoView({behavior: 'smooth'});
+  }
+});
+
+document.querySelector('.scroll-up').addEventListener('click', function() {
+  if (currentIndex > 0) {
+    currentIndex--;
+    sections[currentIndex].scrollIntoView({behavior: 'smooth'});
+  }
+});
